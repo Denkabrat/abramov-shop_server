742df25c553111de24647e2a8b9c2a57880d22cb
Denkabrat <denis46778@gmail.com>
2023-12-06 03:23:46 +0300

    server

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const express = require('express');
 const sequelize = require('./db');
 const PORT = process.env.PORT || 7000;
 const models = require('./models/models');
-const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const cors = require('cors');
+
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use('/api', router);
+app.use(errorHandler);
 
 const allowedOrigins = ['https://abramova.shop', 'https://harmonious-flan-68744d.netlify.app'];
 app.use(cors({
@@ -20,16 +30,6 @@ app.use(cors({
     }
   },
 }));
-
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'static')));
-app.use(fileUpload({}));
-app.use('/api', router);
-app.use(errorHandler);
-
 
 const start = async () => {
     try {
